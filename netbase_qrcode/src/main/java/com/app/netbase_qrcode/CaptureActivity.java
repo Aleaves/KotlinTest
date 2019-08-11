2c53118ab97ca79ddb479ff4a180e8bc21dfaf6c
Aleaves <2849459897@qq.com>
2019-08-11 12:48:21 +0800

    1

---
+++ netbase_qrcode/src/main/java/com/app/netbase_qrcode/CaptureActivity.java
@@ -0,0 +1,23 @@
+package com.app.netbase_qrcode;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+
+public class CaptureActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+}
