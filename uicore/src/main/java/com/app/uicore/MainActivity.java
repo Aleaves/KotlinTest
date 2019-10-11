8c23d0b5c2c49ba990cda107b14f71cfc0f8c072
Aleaves <2849459897@qq.com>
2019-10-11 20:50:55 +0800

    爆炸效果

--- uicore/src/main/java/com/app/uicore/MainActivity.java
+++ uicore/src/main/java/com/app/uicore/MainActivity.java
@@ -1,27 +1,15 @@
 package com.app.uicore;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import com.app.uicore.bezier.BezierView;
-import com.app.uicore.bezier.PathView;
-import com.app.uicore.colorfilter.ColorFilterView;
-import com.app.uicore.view.ComposeView;
-import com.app.uicore.view.PaintView;
-import com.app.uicore.xfermode.EraserView;
+import com.app.uicore.canvas.SplitView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new BezierView(this));
-
+        setContentView(new SplitView(this));
     }
 }
---
+++ uicore/src/main/java/com/app/uicore/canvas/Ball.java
@@ -0,0 +1,18 @@
+package com.app.uicore.canvas;
+
+/**
+ * 粒子封装对象
+ */
+public class Ball {
+
+    public int color;//粒子像素点颜色值
+    public float x;//粒子圆心坐标X
+    public float y;//粒子圆心坐标Y
+    public float r;//粒子半径
+
+    public float vX;//粒子水平方向速度
+    public float vY;//粒子垂直方向速度
+    public float aX;//粒子水平方向加速度
+    public float aY;//粒子垂直方向加速度
+
+}
